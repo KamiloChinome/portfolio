49e4f474684f8dae48fc8d7edec3e3f88d2b921c
Kamilo Chinome <kamiloandresch@gmail.com>
2023-10-15 22:53:59 -0500

    logo animation update

---
+++ lib/UI/screens/home/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/UI/screens/home/desktop_home.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String name = 'home';
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout.builder(
+      desktop: (context) => const DesktopHomeScreen(),
+    );
+  }
+}
