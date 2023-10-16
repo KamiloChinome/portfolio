8d56b4b79292923c74051529ce04808dd89f3c07
Kamilo Chinome <kamiloandresch@gmail.com>
2023-10-15 20:09:34 -0500

    logo animation update

--- lib/UI/screens/splash_screen.dart
+++ lib/UI/screens/splash_screen.dart
@@ -2,34 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:go_router/go_router.dart';
 
-class SplashScreen extends StatefulWidget {
-  static const String name = 'splash';
+class SplashScreen extends StatelessWidget {
+  static const name = 'splash';
   const SplashScreen({Key? key}) : super(key: key);
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(
-      const Duration(seconds: 3),
-      () => context.pushReplacementNamed('home'),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Image.asset(
-      'assets/logoKCH.png',
-      height: 170,
-    )
-                .animate()
-                .scale(duration: const Duration(seconds: 2), curve: Curves.easeInOutQuint)
-                .fade(duration: const Duration(seconds: 2), curve: Curves.easeInOutQuint)));
+      body: Center(
+          child: Stack(
+        children: [
+          Image.asset(
+            'assets/kchLogo1.png',
+            height: 190,
+          )
+              .animate()
+              .fade(duration: const Duration(milliseconds: 1500))
+              .scale(duration: const Duration(milliseconds: 1500), curve: Curves.easeInOutQuint),
+          FutureBuilder(
+            future: animateLogo(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Image.asset(
+                  'assets/kchLogo2.png',
+                  height: 190,
+                )
+                    .animate(
+                      onComplete: (controller) =>
+                          Future.delayed(const Duration(seconds: 1), () => context.pushReplacementNamed('home')),
+                    )
+                    .fade(duration: const Duration(milliseconds: 500));
+              } else {
+                return const SizedBox();
+              }
+            },
+          )
+        ],
+      )),
+    );
+  }
+
+  Future<void> animateLogo() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
   }
 }
